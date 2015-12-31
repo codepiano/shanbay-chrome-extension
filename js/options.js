ac1388bd98c359ad7a98fa8407a008a83b67b9c4
codepiano <codepiano.li@gmail.com>
2015-12-31 23:15:29 +0800

    add option to switch betwwen baidu and shanbay

--- js/options.js
+++ js/options.js
@@ -1,13 +1,15 @@
 function save_options() {
     var wordTranslate = document.getElementById('word_translate').checked;
     var exampleTranslate = document.getElementById('example_translate').checked;
+    var useBaiduCollins = document.getElementById('use_baidu_collins').checked;
     chrome.storage.sync.set({
         wordTranslate: wordTranslate,
-        exampleTranslate: exampleTranslate
-    }, function() {
+        exampleTranslate: exampleTranslate,
+        useBaiduCollins: useBaiduCollins
+    }, function () {
         var status = document.getElementById('status');
         status.textContent = '选项保存成功';
-        setTimeout(function() {
+        setTimeout(function () {
             status.textContent = '';
         }, 750);
     });
@@ -16,10 +18,12 @@ function save_options() {
 function restore_options() {
     chrome.storage.sync.get({
         wordTranslate: true,
-        exampleTranslate: true
-    }, function(items) {
+        exampleTranslate: true,
+        useBaiduCollins: true
+    }, function (items) {
         document.getElementById('word_translate').checked = items.wordTranslate;
         document.getElementById('example_translate').checked = items.exampleTranslate;
+        document.getElementById('use_baidu_collins').checked = items.useBaiduCollins;
     });
 }
 
--- season.js
+++ season.js
@@ -1,13 +1,13 @@
-chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
+chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
     console.log(JSON.stringify(details));
     var headers = details.requestHeaders,
-    blockingResponse = {};
-    for( var i = 0, l = headers.length; i < l; ++i ) {
-        if(headers[i].name === 'Origin' || headers[i].name === 'Referer') {
+        blockingResponse = {};
+    for (var i = 0, l = headers.length; i < l; ++i) {
+        if (headers[i].name === 'Origin' || headers[i].name === 'Referer') {
             headers[i].value = 'http://fanyi.baidu.com/';
             continue;
         }
     }
     blockingResponse.requestHeaders = headers;
     return blockingResponse;
-},{urls: [ "http://fanyi.baidu.com/v2transapi" ]},['requestHeaders','blocking']);
+}, {urls: ["http://fanyi.baidu.com/v2transapi"]}, ['requestHeaders', 'blocking']);
