0d19bf1a173e4c2aa4a3b060009b032be03d4c41
codepiano <codepiano.li@gmail.com>
2016-01-19 13:49:09 +0800

    exclude useless images

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd .. && zip -r shanbay.zip shanbay --exclude \*.git\* \*.gitignore\* \*.zip\* \*.DS_Store\* \*package.sh\* && mv shanbay.zip shanbay
+cd .. && zip -r shanbay.zip shanbay --exclude \*.git\* \*install.jpg \*intro.jpg \*.gitignore\* \*.zip\* \*.DS_Store\* \*package.sh\* && mv shanbay.zip shanbay
